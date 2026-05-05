5aed9df7a2a7382b7a2d829d77354faf3ddd4b18
Leon Scharnweber <git.hub.leon@gmail.com>
2026-05-05 17:09:32 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+vSGgtNamensdienst
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ src/vSGgtNamensdienst_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc vSGgtNamensdienst public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(vSGgtNamensdienst_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    vSGgtNamensdienst_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
